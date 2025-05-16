0b5926aeca2fcdab9ecf48bdb4cb953b05528d16
pralhad gadlegave <pralhadgadlegave55@gmail.com>
2025-05-16 22:18:59 +0530

    Initial commit

---
+++ src/test/java/com/resumeAi/resume_upload_ai/ResumeUploadAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeAi.resume_upload_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeUploadAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
